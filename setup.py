767f6210857c17975137e522d4f748342f5ecb9b
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-01-14 20:12:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'e-mail facility',
+    'author': 'Adisakshya Chauhan',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'adisakshya chauhan',
+    'version': '1.0',
+    'install_requires': [],
+    'packages': [],
+    'scripts': ['bin/pymail.py'],
+    'name': 'pymail'
+    }
+
+setup(**config)
