618b3d7d8d683d4c8f12b306219a0680d03286eb
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-01-23 19:20:59 +0530

    added code for attachment

--- bin/pymail.py
+++ bin/pymail.py
@@ -1,76 +1,72 @@
-import smtplib
-from sys import argv
-from requests import get
-from string import Template
+import email, smtplib, ssl
+import sys
+from email import encoders
+from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-MY_ADDRESS = argv[1]
-PASSWORD = argv[2]
+def get_sender_details():
 
-def get_contacts(filename):
-    """
-    Return two lists names, emails containing names and email addresses
-    read from a file specified by filename.
-    """
-    
-    names = []
-    emails = []
-    with open(filename, mode='r', encoding='utf-8') as contacts_file:
-        for a_contact in contacts_file:
-            names.append(a_contact.split()[0])
-            emails.append(a_contact.split()[1])
-    return names, emails
+	return sys.argv[1], sys.argv[2]
 
-def read_template(filename):
-    """
-    Returns a Template object comprising the contents of the 
-    file specified by filename.
-    """
-    
-    with open(filename, 'r', encoding='utf-8') as template_file:
-        template_file_content = template_file.read()
-    return Template(template_file_content)
+def get_receiver_email_list():
+
+	receivers_list = []
+
+	while True:
+
+		receiver_email = input('Enter Reveiver Email: ')
+		receivers_list.append(receiver_email)
+		print('Receivers List: ', receivers_list)
+
+		if input('Want to add more receivers? (Y/N): ') in ['n', 'N']:
+
+			break
+
+	return receivers_list		
 
 def main():
-    names, emails = get_contacts('my_contacts.txt') # read contacts
-    message_template = read_template('message.txt')
 
-    # set up the SMTP server
-    s = smtplib.SMTP(host=get('http://api.ipify.org').text, port=465)
-    s.starttls()
-    s.login(MY_ADDRESS, PASSWORD)
+	sender_email, password = get_sender_details()
+	receivers_email_list = get_receiver_email_list()
+	subject = input('Enter Subject: ')
+	body = input('Enter Body: ')
 
-    # For each contact, send the email:
-    for name, email in zip(names, emails):
-        msg = MIMEMultipart()       # create a message
+	# Create a multipart message and set headers
+	message = MIMEMultipart()
+	message["From"] = sender_email
+	message["Subject"] = subject
+	#filename = "github.pdf"  # In same directory as script
 
-        # add in the actual person name to the message template
-        message = message_template.substitute(PERSON_NAME=name.title())
+	for i in range(len(receivers_email_list)):
 
-        # Prints out the message body for our sake
-        print(message)
+		message["To"] = receivers_email_list[i]
+		
+		# Add body to email
+		message.attach(MIMEText(body, "plain"))
 
-        # setup the parameters of the message
-        msg['From']=MY_ADDRESS
-        msg['To']=email
-        msg['Subject']="This is TEST"
-        
-        # add in the message body
-        msg.attach(MIMEText(message, 'plain'))
-        
-        # send the message via the server set up earlier.
-        s.send_message(msg)
-        del msg
-        
-    # Terminate the SMTP session and close the connection
-    s.quit()
+		if input('Any attachments?(Y/N): ') in ['y', 'Y']:
+			
+			filename = input('Enter filename for attachments: ')
+			with open(filename, 'rb') as attachment:
+				part = MIMEBase("application","octet-stream")
+				part.set_payload(attachment.read())
 
-def usage():
-    print('Usage: py pymail.py [e-mail] [password]')
-    
+			# Add header as key/value pair to attachment part
+			part.add_header(
+    			"Content-Disposition",
+    			f"attachment; filename= {filename}",
+			)
+
+		# Add attachment to message and convert message to string
+		message.attach(part)
+		text = message.as_string()
+
+		# Log in to server using secure context and send email
+		context = ssl.create_default_context()
+		with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+			server.login(sender_email, password)
+			server.sendmail(sender_email, receivers_email_list[i], text)
+
 if __name__ == '__main__':
-    try:
-        main()
-    except:
-        usage()
+	main()
